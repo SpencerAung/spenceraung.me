95a8bb47334a73db4e3d06ec50dd9fb4c3eeca4e
SpencerAung <10285698+SpencerAung@users.noreply.github.com>
2019-07-10 23:45:29 +0900

    Upgrade typograhpy

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -2,5 +2,6 @@ import Typography from 'typography';
 import irvingTheme from 'typography-theme-irving';
 
 const typography = new Typography(irvingTheme);
+const { rhythm, scale } = typography;
 
-export default typography;
+export { rhythm, scale, typography as default };
