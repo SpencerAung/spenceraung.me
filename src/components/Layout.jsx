3f2fd8bf0e7e6d0b315b1279cac0b9459e8a1855
SpencerAung <10285698+SpencerAung@users.noreply.github.com>
2019-07-27 00:14:31 +0900

    Organzie SEO

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,5 @@
 import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 import styled, { ThemeProvider } from 'styled-components'
 
@@ -40,15 +39,6 @@ const Layout = ({ children }) => (
     `}
     render={(data) => (
       <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' }
-          ]}
-        >
-          <html lang='en' />
-        </Helmet>
         <ThemeProvider theme={theme}>
           <Fragment>
             <GlobalStyle />
