0e17b7b7c2e60448c7574e586853ee1408a1c7d8
SpencerAung <10285698+SpencerAung@users.noreply.github.com>
2020-01-02 11:27:49 +0900

    feat: update index page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,51 +5,7 @@ import SEO from '../components/SEO'
 const IndexPage = () => (
   <Layout>
     <SEO />
-    <h1>
-      Hello!{' '}
-      <span role='img' aria-label='waving hand'>
-        👋
-      </span>
-    </h1>
-    <p>
-      My name is Spencer Aung. I am a JavaScript developer at{' '}
-      <a
-        href='https://www.korbit.co.kr'
-        target='_blank'
-        rel='noreferrer noopener'
-      >
-        Korbit.
-      </a>
-    </p>
-    <p>
-      I work from home and enjoy life with{' '}
-      <a
-        href='https://www.instagram.com/mr__garu/'
-        target='_blank'
-        rel='noreferrer noopener'
-      >
-        Mr. Garu
-      </a>
-      , my favorite cat in the whole world.
-      <br />
-      When I am not making apps, I am either on Netflix or reading a book.
-    </p>
-    <h3>2019 Missions</h3>
-    <ul>
-      <li>Improve Functional Programming skill</li>
-      <li>Walk at least 5 km a day</li>
-      <li>Make a product prototype</li>
-      <li>Learn Korean</li>
-    </ul>
-    <h3>Reach me at</h3>
-    <p>
-      <a href='mailto:aung.spencer@gmail.com'>
-        <span role='img' aria-label='email icon'>
-          📧
-        </span>{' '}
-        aung.spencer@gmail.com
-      </a>
-    </p>
+    <h1>Happy New Year!</h1>
   </Layout>
 )
 
