5367f705f06ad353524f591401a08bb4a663644f
SpencerAung <10285698+SpencerAung@users.noreply.github.com>
2020-01-14 10:40:23 +0900

    fix: restore index page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,44 @@ import SEO from '../components/SEO'
 const IndexPage = () => (
   <Layout>
     <SEO />
-    <h1>Happy New Year!</h1>
+    <h1>
+      Hello!{' '}
+      <span role='img' aria-label='waving hand'>
+        👋
+      </span>
+    </h1>
+    <p>
+      My name is Spencer Aung. I am a JavaScript developer at{' '}
+      <a
+        href='https://www.korbit.co.kr'
+        target='_blank'
+        rel='noreferrer noopener'
+      >
+        Korbit.
+      </a>
+    </p>
+    <p>
+      I work from home and enjoy life with{' '}
+      <a
+        href='https://www.instagram.com/mr__garu/'
+        target='_blank'
+        rel='noreferrer noopener'
+      >
+        Mr. Garu
+      </a>
+      , my favorite cat in the whole world.
+      <br />
+      When I am not making apps, I am either on Netflix or reading a book.
+    </p>
+    <h3>Reach me at</h3>
+    <p>
+      <a href='mailto:aung.spencer@gmail.com'>
+        <span role='img' aria-label='email icon'>
+          📧
+        </span>{' '}
+        aung.spencer@gmail.com
+      </a>
+    </p>
   </Layout>
 )
 
