6e9af2b3a546416417cc4eff59c95827b6f8c059
SpencerAung <10285698+SpencerAung@users.noreply.github.com>
2022-10-13 14:40:04 +0900

    Update new project card content

--- src/components/NewProjectCard.js
+++ src/components/NewProjectCard.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import styled from 'styled-components'
+// import React from 'react'
+// import styled from 'styled-components'
 
-const Card = styled.div`
-  margin: 4rem 0;
-  border: 2px solid ${(props) => props.theme.red};
-  background-color: ${(props) => props.theme.white};
-  padding: 2rem;
-
-  h3 {
-    margin: 0;
-    font-weight: 700;
-  }
-
-  a {
-    font-weight: 700;
-  }
-
-  p {
-    margin: 0;
-  }
-`
+// const Card = styled.div`
+//   margin: 4rem 0;
+//   border: 2px solid ${(props) => props.theme.red};
+//   background-color: ${(props) => props.theme.white};
+//   padding: 2rem;
+//
+//   h3 {
+//     margin: 0;
+//     font-weight: 700;
+//   }
+//
+//   a {
+//     font-weight: 700;
+//   }
+//
+//   p {
+//     margin: 0;
+//   }
+// `
 
 /* COVID-19 project card */
 // export default () => (
@@ -30,18 +30,22 @@ const Card = styled.div`
 // )
 //
 
-export default () => (
-  <Card>
-    <h3>On Feb 1, 2021, military coup happened in my home country, Myanmar.</h3>
-    <p>
-      Democracy is no way in sight and our basic human rights have been revoked
-      to serve the benefits of the military junta.
-    </p>
-    <p>
-      I deeply condemned the acts of military junta led by Gen. Min Aung Hlaing
-      and his associates. I hereby promise that I will do everything in my power
-      to stop them and support everyone who has been fighting them since day
-      one.
-    </p>
-  </Card>
-)
+/* MM Coup */
+// export default () => (
+//   <Card>
+//     <h3>On Feb 1, 2021, military coup happened in my home country, Myanmar.</h3>
+//     <p>
+//       Democracy is no way in sight and our basic human rights have been revoked
+//       to serve the benefits of the military junta.
+//     </p>
+//     <p>
+//       I deeply condemned the acts of military junta led by Gen. Min Aung Hlaing
+//       and his associates. I hereby promise that I will do everything in my power
+//       to stop them and support everyone who has been fighting them since day
+//       one.
+//     </p>
+//   </Card>
+// )
+//
+
+export default () => null
